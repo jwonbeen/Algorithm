dac734609abdac2704c2475397798be153e8b14a
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-08 22:47:01 +0900

    [Passed] 길이 단위 변환하기 (97ms, 9MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double ft = 30.48;
+        int mi = 160934;
+        double a = 9.2;
+        double b = 1.3;
+
+        System.out.printf("%.1fft = %.1fcm \n%.1fmi = %.1fcm",a, a*ft, b, b*mi);
+    }
+}
