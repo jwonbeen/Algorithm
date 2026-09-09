6c21a0ed7b4bf745d4ea1369dd074f1429564a99
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-09 09:13:18 +0900

    [Passed] 변수 값 교체하기 3 (93ms, 9MB)

--- trail1/변수 값 교체하기 3/replacing-variable-values-3.java
+++ trail1/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        int a = 3, b = 5, tmp = 0;
-        tmp = a;
+        int a = 3, b = 5, tmp = a;
         a = b;
         b = tmp;
         System.out.println(a + "\n" + b);
