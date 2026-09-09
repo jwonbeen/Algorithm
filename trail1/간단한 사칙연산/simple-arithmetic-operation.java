951d63b53a05683cba9f0d434fa35936b44b9580
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-09 13:09:05 +0900

    [Passed] 간단한 사칙연산 (129ms, 10MB)

---
+++ trail1/간단한 사칙연산/simple-arithmetic-operation.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        // 출력
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}
