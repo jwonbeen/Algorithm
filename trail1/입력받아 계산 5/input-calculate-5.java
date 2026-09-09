d817a897f831f6cf878c3443a4be1a193834ec7c
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-09 09:44:53 +0900

    [Passed] 입력받아 계산 5 (136ms, 10MB)

---
+++ trail1/입력받아 계산 5/input-calculate-5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // 출력
+        System.out.print(a + b);
+    }
+}
