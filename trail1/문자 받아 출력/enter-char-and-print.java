feb1922984e292d0d8b3a2102cae59edefc36468
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-09 10:03:19 +0900

    [Passed] 문자 받아 출력 (123ms, 10MB)

---
+++ trail1/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력
+        char a = sc.next().charAt(0);
+
+        // 출력
+        System.out.println(a);
+    }
+}
