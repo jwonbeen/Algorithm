1c9c15c4d4cd235562249c143262dc1d06434e72
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-09 10:04:38 +0900

    [Passed] 문자열 받아 출력 (127ms, 10MB)

---
+++ trail1/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        String s = sc.next();
+
+        //출력
+        System.out.println(s);
+    }
+}
