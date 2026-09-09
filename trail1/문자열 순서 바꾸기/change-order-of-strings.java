4ed2b4990c2da84ec3bc0b31773bd51a6f9adb3c
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-09 10:09:42 +0900

    [Passed] 문자열 순서 바꾸기 (134ms, 10MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        String S = sc.next();
+        String T = sc.next();
+
+        System.out.printf("%s\n%s", T, S);
+    }
+}
