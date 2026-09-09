acd98bb25491a924577dde1a2818402478142564
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-09 09:12:48 +0900

    [Passed] 변수 값 교체하기 3 (94ms, 9MB)

---
+++ trail1/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3, b = 5, tmp = 0;
+        tmp = a;
+        a = b;
+        b = tmp;
+        System.out.println(a + "\n" + b);
+    }
+}
