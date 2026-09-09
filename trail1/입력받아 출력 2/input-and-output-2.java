1688096c9ec6650166c2a0c94248486fbb592c4e
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-09 10:45:56 +0900

    [Passed] 입력받아 출력 2 (128ms, 10MB)

--- trail1/입력받아 출력 2/input-and-output-2.java
+++ trail1/입력받아 출력 2/input-and-output-2.java
@@ -4,10 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // 변수 설정 및 입력
-        sc.useDelimiter("[-\\s]");
-        String a = sc.next();
-        String b = sc.next();
+        String s = sc.next().replace("-","");
         // 출력
-        System.out.print(a + b);
+        System.out.print(s);
     }
 }
