110a7e1f20af41e32e04cd3825d62241fb6e212f
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-09 09:23:16 +0900

    [Passed] 입력받아 계산 (132ms, 10MB)

---
+++ trail1/입력받아 계산/input-calculate.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 값 변경
+        int a = sc.nextInt() + 2;
+
+        // 출력
+        System.out.print(a);
+    }
+}
