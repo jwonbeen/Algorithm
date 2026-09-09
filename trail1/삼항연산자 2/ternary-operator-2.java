5dcb7fd28e9ef64eba8b89a817ce811a62773725
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-09 16:01:43 +0900

    [Passed] 삼항연산자 2 (139ms, 10MB)

---
+++ trail1/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력 및 출력
+        System.out.println(sc.nextInt() == 1 ? "t" : "f");
+    }
+}
