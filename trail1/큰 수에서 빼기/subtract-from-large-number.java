0e1f7c52d9c4d15eb52250ef2605b695e64ca2dc
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-09 14:39:55 +0900

    [Passed] 큰 수에서 빼기 (132ms, 10MB)

---
+++ trail1/큰 수에서 빼기/subtract-from-large-number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        // 출력
+        System.out.print(Math.abs(A - B));
+    }
+}
