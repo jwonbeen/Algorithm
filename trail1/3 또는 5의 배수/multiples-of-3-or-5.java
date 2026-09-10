707233eace515d0913f245c751ab6b3e3544f0b2
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-10 10:36:08 +0900

    [Passed] 3 또는 5의 배수 (129ms, 10MB)

---
+++ trail1/3 또는 5의 배수/multiples-of-3-or-5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int A = sc.nextInt();
+
+        // 조건 출력
+        if (A % 3 == 0) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+        if (A % 5 == 0) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+}
