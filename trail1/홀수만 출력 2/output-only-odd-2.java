8380836cd17e6f503ed297400ef0f5a995db54a9
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-11 14:58:32 +0900

    [Passed] 홀수만 출력 2 (130ms, 10MB)

---
+++ trail1/홀수만 출력 2/output-only-odd-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        // 출력
+        for (int i = A; i > B - 1; i -= 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
