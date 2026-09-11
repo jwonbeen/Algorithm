336169340b3dba361d871cf080629aa4333a6795
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-11 14:05:32 +0900

    [Passed] 윤년인가 (134ms, 10MB)

---
+++ trail1/윤년인가/is-leap-year.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int Y = sc.nextInt();
+
+        // 출력
+        if (Y % 4 == 0 && Y % 100 != 0 || Y % 400 == 0) {
+            System.out.print("true");
+        }
+        else {
+            System.out.print("false");
+        }
+    }
+}
