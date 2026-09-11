ec6b343a2a4e17eac0cd6ca882b5effb192dab5c
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-11 14:51:45 +0900

    [Passed] 홀수만 출력 (130ms, 10MB)

--- trail1/홀수만 출력/output-only-odd.java
+++ trail1/홀수만 출력/output-only-odd.java
@@ -7,10 +7,9 @@ public class Main {
         // 변수 선언 및 입력
         int A = sc.nextInt();
         int B = sc.nextInt();
-        int start = A % 2 == 0 ? A + 1 : A;
 
         // 출력
-        for (int i = start; i < B + 1; i += 2) {
+        for (int i = A; i < B + 1; i += 2) {
             System.out.print(i + " ");
         }
     }
