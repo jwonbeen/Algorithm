92242f01aa02814a466b18fdd635d2da01229a77
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-11 13:46:40 +0900

    [Passed] 남녀노소 구분짓기 (130ms, 10MB)

---
+++ trail1/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int sex = sc.nextInt();
+        int age = sc.nextInt();
+        boolean isAdult = (age >= 19);
+        // 출력
+        if (sex == 0 && isAdult) System.out.print("MAN");
+        else if (sex == 0) System.out.print("BOY");
+        else if (isAdult) System.out.print("WOMAN");
+        else System.out.print("GIRL");
+    }
+}
