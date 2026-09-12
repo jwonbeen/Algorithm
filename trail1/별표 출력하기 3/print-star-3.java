567c113ef5756a33b9b0cd9729a2697e2f6f3fc5
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-12 13:02:37 +0900

    [Passed] 별표 출력하기 3 (126ms, 10MB)

--- trail1/별표 출력하기 3/print-star-3.java
+++ trail1/별표 출력하기 3/print-star-3.java
@@ -4,15 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // 변수 선언 및 입력
         int N = sc.nextInt();
 
+        sc.close();
+
+        // 출력
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < i; j ++) {
-                System.out.print("  ");
-            }
-            int stars = (2 * N - 1) - 2 * i;
-            for (int k = 0; k < stars; k++) {
-                System.out.print("* ");
+            int stars = (2 * N - 1) - i;
+            for (int j = 0; j < stars; j++) {
+                if (i > j) {
+                    System.out.print("  ");
+                }
+                else {
+                    System.out.print("* ");
+                }
             }
             System.out.println();
         }
