1bb3b0693384072655b9b2fa68bf3bf8d4774419
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-12 13:27:50 +0900

    [Passed] 두 개의 직각삼각형 (134ms, 10MB)

---
+++ trail1/두 개의 직각삼각형/two-right-triangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int N = sc.nextInt();
+
+        sc.close();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2 * N; j++) {
+                if ((N - i) <=  j && j < (N + i)) {
+                    System.out.print(" ");
+                }
+                else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+} 
