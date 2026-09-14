43bb54f00d28ffe1d68c1d37d3624458fcf0a7ab
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-15 08:36:01 +0900

    [Passed] 별표 출력하기 11 (132ms, 10MB)

---
+++ trail1/별표 출력하기 11/print-star-11.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.close();
+
+        int size = 2 * N + 1;
+
+        for (int i = 0; i < size; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < size; j++) {
+                    System.out.print("* ");
+                }
+            }
+            else {
+                for (int j = 0; j < size; j++) {
+                    if (j % 2 == 0) {
+                        System.out.print("* ");
+                    }
+                    else {
+                        System.out.print("  ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
