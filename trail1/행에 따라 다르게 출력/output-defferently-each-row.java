acad6d75ccd185ea05e476475f58943536525f7b
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-17 23:13:54 +0900

    [Passed] 행에 따라 다르게 출력 (124ms, 10MB)

---
+++ trail1/행에 따라 다르게 출력/output-defferently-each-row.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i % 2 == 0) {
+                    System.out.print(++count + " ");
+                }
+                else {
+                    count += 2;
+                    System.out.print(count + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
