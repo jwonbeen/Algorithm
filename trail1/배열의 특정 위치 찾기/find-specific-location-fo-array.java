f53eb5dbbcc3afc1dd353f0191c1ce1a9d070d2a
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-20 15:48:56 +0900

    [Passed] 배열의 특정 위치 찾기 (138ms, 10MB)

---
+++ trail1/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sumOf2s = 0;
+        int sumOf3s = 0;
+        int cnt = 0;
+
+        for (int i = 1; i < 11; i++) {
+            int dummy = sc.nextInt();
+            if (i % 2 == 0) {
+                sumOf2s += dummy;
+            }
+            if (i % 3 == 0) {
+                sumOf3s += dummy;
+                cnt++;
+            }
+        }
+
+        System.out.print(sumOf2s + " ");
+        System.out.printf("%.1f", (double)sumOf3s / cnt);
+    }
+}
