f5f879bd253a8af6386f369ad7e5c1ffb55b8243
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-20 14:44:50 +0900

    [Passed] 특정 수 도달하기 (129ms, 10MB)

---
+++ trail1/특정 수 도달하기/reaching-specific-number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int num = sc.nextInt();
+
+            if (num >= 250) {
+                break;
+            }
+            sum += num;
+            cnt++;
+        }
+        sc.close();
+
+        System.out.printf("%d %.1f", sum, (double)sum / cnt);
+    }
+}
