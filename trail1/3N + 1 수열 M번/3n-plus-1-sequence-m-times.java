167d8c39ad046f48bba354a31961b2418d8bba02
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-20 10:57:41 +0900

    [Passed] 3N + 1 수열 M번 (125ms, 10MB)

--- trail1/3N + 1 수열 M번/3n-plus-1-sequence-m-times.java
+++ trail1/3N + 1 수열 M번/3n-plus-1-sequence-m-times.java
@@ -13,12 +13,11 @@ public class Main {
             while (N > 1) {
                 if (N % 2 == 0) {
                     N /= 2;
-                    cnt++;
                 }
                 else {
                     N = 3 * N + 1;
-                    cnt++;
                 }
+                cnt++;
             }
             System.out.println(cnt);
         }
