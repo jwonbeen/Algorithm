4e1717a5eaac7a57bd503e27fd68af0a4c0d31df
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-21 15:54:57 +0900

    [Passed] 특정 원소들의 합 (128ms, 10MB)

---
+++ trail1/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int val = sc.nextInt();
+                if (j <= i) {
+                    sum += val;
+                }
+            }
+        }
+        sc.close();
+
+        System.out.println(sum);
+    }
+}
