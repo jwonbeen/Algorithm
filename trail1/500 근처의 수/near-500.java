8e9525188d9a5f72e1f3db03e5689effa2f904c8
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-21 14:51:50 +0900

    [Passed] 500 근처의 수 (130ms, 10MB)

--- trail1/500 근처의 수/near-500.java
+++ trail1/500 근처의 수/near-500.java
@@ -4,18 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int under500 = 0;
-        int up500 = 1000;
+        int under500Max = 0;
+        int over500Min = 1000;
 
         for (int i = 0; i < 10; i++) {
             int n = sc.nextInt();
-            if (n > 500 && n < up500) {
-                up500 = n;
+            if (n > 500 && n < over500Min) {
+                over500Min = n;
             }
-            else if (n < 500 && n > under500) {
-                under500 = n;
+            else if (n < 500 && n > under500Max) {
+                under500Max = n;
             }
         }
-        System.out.print(under500 + " " + up500);
+        System.out.print(under500Max + " " + over500Min);
     }
 }
