9076964ac07e510f7b61a9e518bab025ffe0cb1b
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-22 10:02:48 +0900

    [Passed] 파스칼의 삼각형 (129ms, 10MB)

---
+++ trail1/파스칼의 삼각형/pascals-triangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                if (j == 0 || j == i) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+                }
+                
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
