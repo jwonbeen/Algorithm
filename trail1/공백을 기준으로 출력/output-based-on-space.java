ad858ae5b0609662d856b4284e1a52473765a165
jwonbeen <rn4d8c2vkn@privaterelay.appleid.com>
2026-09-22 10:31:27 +0900

    [Passed] 공백을 기준으로 출력 (119ms, 10MB)

---
+++ trail1/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine().replace(" ", "");
+        String str2 = sc.nextLine().replace(" ", "");
+
+        System.out.print(str1 + str2);
+    }
+}
